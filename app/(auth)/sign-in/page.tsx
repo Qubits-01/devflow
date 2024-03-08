458d8e9da4525c5e6a43bb4165353344cb4e07af
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-03-08 12:01:12 +0800

    feat: implement basic auth

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SignIn = () => {
-  console.log("[ SignIn ] server side rendering");
-
-  return <div>Sign In</div>;
-};
-
-export default SignIn;
--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen">
+      Home
+      <UserButton afterSignOutUrl="/"></UserButton>
+    </div>
+  );
 };
 
 export default Home;
